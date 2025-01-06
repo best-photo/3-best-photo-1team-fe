2771264e37970f66c94b3bf75420992a91f84d7e
Daewony <alexnori.com@gmail.com>
2025-01-06 17:06:42 +0900

    feat: GNB 컴포넌트 구현

---
+++ src/components/layout/GNB/GNBLeftSection.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import LogoImage from '@/public/images/logo.png';
+
+const GNBLeftSection = () => {
+  return (
+    <div className='relative w-[111px] h-5 lg:w-[140px] lg:h-[25px]'>
+      <Link href='/'>
+        <Image
+          src={LogoImage}
+          fill
+          alt='로고'
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default GNBLeftSection;
