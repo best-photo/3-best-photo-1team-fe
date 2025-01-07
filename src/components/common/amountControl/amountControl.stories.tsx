843d17cf4fa11c6096c49cabe38ff0195d78d98a
heonq <gkagjsrb@naver.com>
2025-01-07 15:30:29 +0900

    feat: 수량 컨트롤 컴포넌트 구현

---
+++ src/components/common/amountControl/amountControl.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryFn } from '@storybook/react';
+import AmountControl from './amountControl';
+
+export default {
+  title: 'common/atom/AmountControl',
+  component: AmountControl,
+  tags: ['autodocs'],
+} satisfies Meta<typeof AmountControl>;
+
+const Template: StoryFn<typeof AmountControl> = (args) => (
+  <AmountControl {...args} />
+);
+
+export const Amount = Template.bind({});
+
+Amount.args = {
+  amount: 1,
+};
