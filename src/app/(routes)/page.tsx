990e177044ffaf50b847854bfa9c35b6ef49af58
soohwanpak <qoiuytnghks4@gmail.com>
2025-01-08 11:44:01 +0900

    공통페이지, 판매자모달 포토카드 임시적용

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from 'react';
 import MarketplaceHeader from '../../components/marketplace/MarketplaceHeader';
 import MarketplaceSearchBox from '../../components/marketplace/MarketplaceSearchBox';
+import { mockPhotoCards } from '@/src/components/marketplace/mockData';
+import PhotoCardListItem from '@/src/components/common/photoCard/organisms/photoCardListItem/photoCardListItem';
 
 export default function Home() {
   const [isAlertVisible, setAlertVisible] = useState(false);
@@ -12,18 +14,29 @@ export default function Home() {
   const userId: string | null = '1';
 
   return (
-    <div className='min-h-screen pt-[60px]'>
-      <MarketplaceHeader
-        isAlertVisible={isAlertVisible}
-        setAlertVisible={setAlertVisible}
-        isLoginAlertVisible={isLoginAlertVisible}
-        setIsLoginAlertVisible={setIsLoginAlertVisible}
-        isProductVisible={isProductVisible}
-        setProductVisible={setProductVisible}
-        userId={userId}
-      />
-      <div className='border-b border-white w-[1480px] mx-auto mt-[20px]'></div>
-      <MarketplaceSearchBox />
-    </div>
+    <>
+      <div className='pt-[60px]'>
+        <MarketplaceHeader
+          isAlertVisible={isAlertVisible}
+          setAlertVisible={setAlertVisible}
+          isLoginAlertVisible={isLoginAlertVisible}
+          setIsLoginAlertVisible={setIsLoginAlertVisible}
+          isProductVisible={isProductVisible}
+          setProductVisible={setProductVisible}
+          userId={userId}
+        />
+        <div className='border-b border-white w-[1480px] mx-auto mt-[20px]'></div>
+        <MarketplaceSearchBox />
+      </div>
+
+      <div className='flex gap-[80px] flex-wrap w-[1480px] mx-auto pt-[60px] mb-[100px]'>
+        {mockPhotoCards.map((card, index) => (
+          <PhotoCardListItem
+            key={index}
+            {...card}
+          />
+        ))}
+      </div>
+    </>
   );
 }
