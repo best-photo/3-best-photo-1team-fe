07468d8003ecb1e521f157388b9eb7565c751240
heonq <gkagjsrb@naver.com>
2025-01-08 09:21:41 +0900

    refactor: divder 컴포넌트 리뷰사항 반영

--- src/components/common/photoCard/atoms/divider/verticalDivider.tsx
+++ src/components/common/photoCard/atoms/divider/verticalDivider.tsx
@@ -1,5 +1,6 @@
 import cn from '@/src/utils/cn';
+import { DividerProps } from './divider.types';
 
-export default function VerticalDivider({ className }: { className?: string }) {
+export default function VerticalDivider({ className }: DividerProps) {
   return <span className={cn('text-gray-400', className)}>|</span>;
 }
