5e18726856f5610170029ab729d1c278d15c52b0
heonq <gkagjsrb@naver.com>
2025-01-09 17:08:24 +0900

    feat: useScreenWidth 훅 수정

--- src/hooks/useScreenWidth.ts
+++ src/hooks/useScreenWidth.ts
@@ -2,18 +2,14 @@ import { useEffect, useState } from 'react';
 import { MEDIA_QUERY } from '@/src/constants/mediaQuery';
 
 const useMediaQuery = (query: string) => {
-  const isClient = typeof window !== 'undefined';
-
-  if (!isClient) return MEDIA_QUERY.value.large;
-
   const [matches, setMatches] = useState(() => {
     if (typeof window === 'undefined') return false;
     return window.matchMedia(query).matches;
   });
 
   useEffect(() => {
-    if (typeof window === 'undefined') return;
     const mediaQuery = window.matchMedia(query);
+    setMatches(mediaQuery.matches);
 
     const handleMediaQuery = (e: MediaQueryListEvent) => {
       setMatches(e.matches);
@@ -28,13 +24,11 @@ const useMediaQuery = (query: string) => {
 };
 
 const useScreenWidth = () => {
-  const largeMediaQuery = useMediaQuery(MEDIA_QUERY.query.large);
   const mediumMediaQuery = useMediaQuery(MEDIA_QUERY.query.medium);
   const smallMediaQuery = useMediaQuery(MEDIA_QUERY.query.small);
 
   if (smallMediaQuery) return MEDIA_QUERY.value.small;
   if (mediumMediaQuery) return MEDIA_QUERY.value.medium;
-  if (largeMediaQuery) return MEDIA_QUERY.value.large;
   return MEDIA_QUERY.value.large;
 };
 
