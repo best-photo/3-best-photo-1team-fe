0e2fad58051b26363ccb4a530e536389381450af
heonq <gkagjsrb@naver.com>
2025-01-09 09:38:04 +0900

    chore: storyobook 설정 변경
    - preview.ts를 preview.tsx로 수정
    - decorators에 앱라우터 컨텍스트 추가:
    useRouter를 사용하는 컴포넌트 스토리 작성을 위한 추가

---
+++ .storybook/preview.tsx
@@ -0,0 +1,25 @@
+import type { Preview } from "@storybook/react";
+import './setting.css';
+import {
+  AppRouterContext,
+  type AppRouterInstance
+} from "next/dist/shared/lib/app-router-context.shared-runtime";
+import React from "react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [(Story)=> (
+    <AppRouterContext.Provider value={{} as AppRouterInstance}>
+      <Story/>
+    </AppRouterContext.Provider>
+  )]
+};
+
+export default preview;
