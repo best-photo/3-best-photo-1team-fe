b5a937c91fd5184671f927f42013078f9f03e012
heonq <gkagjsrb@naver.com>
2025-01-14 16:22:34 +0900

    reafctor: FilterModalHeader 컴포넌트 리팩토링

--- src/components/common/filterModal/molecules/categoryGroup/categoryGroup.tsx
+++ src/components/common/filterModal/molecules/categoryGroup/categoryGroup.tsx
@@ -5,11 +5,15 @@ export default function CategoryGroup() {
   const { categories, selectedCategory, setSelectedCategory } =
     useFilterStore();
 
+  if (!categories.length) {
+    return null;
+  }
+
   return (
     <div className='flex gap-6 border-b-gray-500 border-b text-[14px]'>
       {categories.map((category) => (
         <FilterCategory
-          key={category?.queryString}
+          key={category.queryString}
           selectedCategory={selectedCategory}
           category={category}
           setSelectedCategory={setSelectedCategory}
--- src/components/common/filterModal/molecules/filterModalHeader/filterModalHeader.tsx
+++ src/components/common/filterModal/molecules/filterModalHeader/filterModalHeader.tsx
@@ -6,7 +6,7 @@ export default function FilterModalHeader() {
   const setModalOpen = useFilterStore((state) => state.setModalOpen);
 
   return (
-    <div className='relative flex p-[15px] items-center justify-center'>
+    <header className='relative flex p-[15px] items-center justify-center'>
       <h1>필터</h1>
       <button
         className='absolute right-[15px]'
@@ -17,8 +17,9 @@ export default function FilterModalHeader() {
           alt='닫기 버튼'
           width={24}
           height={24}
+          priority
         />
       </button>
-    </div>
+    </header>
   );
 }
