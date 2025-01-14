94039c44124a023971649195119c17cbac8e17eb
soohwanpak <qoiuytnghks4@gmail.com>
2025-01-14 13:20:00 +0900

    fix:searchInput 공통컴포넌트 수정

--- src/components/common/CommonSearchBox/SearchInput.tsx
+++ src/components/common/CommonSearchBox/SearchInput.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 interface SearchInputProps {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onSearchClick: () => void;
   placeholder?: string;
   className?: string;
 }
@@ -13,18 +14,27 @@ interface SearchInputProps {
 const SearchInput: React.FC<SearchInputProps> = ({
   value,
   onChange,
+  onSearchClick,
   placeholder = '검색',
   className = '',
 }) => {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onSearchClick();
+    }
+  };
+
   return (
     <div className={`relative w-full h-[50px] ${className}`}>
       <input
         placeholder={placeholder}
         value={value}
         onChange={onChange}
+        onKeyDown={handleKeyDown}
         className={`w-full h-full p-[13px_20px] gap-[10px] rounded-[2px] border border-[var(--color-gray-200)] bg-[var(--color-black)] text-[var(--color-white)] pr-[30px] outline-none`}
       />
       <Image
+        onClick={onSearchClick}
         src='/icons/search.svg'
         alt='Search Icon'
         className='absolute right-[10px] top-1/2 transform -translate-y-1/2'
