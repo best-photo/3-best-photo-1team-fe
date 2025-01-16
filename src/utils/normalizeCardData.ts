9c7e6a3fd350502f773a8e901b94d36203726cdd
soohwanpak <qoiuytnghks4@gmail.com>
2025-01-16 17:22:38 +0900

    fix : 판매모달 수정2

--- src/utils/normalizeCardData.ts
+++ src/utils/normalizeCardData.ts
@@ -1,47 +1,47 @@
-// import {Grades, Genres} from '@/src/components/common/photoCard/types'
+import {Grades, Genres} from '@/src/components/common/photoCard/types'
 
-// // utils/normalizeCardData.ts
-// export const normalizeGrade = (grade: string): Grades => {
-//   switch (grade.toUpperCase()) {
-//     case 'COMMON':
-//       return 'common'; 
-//     case 'RARE':
-//       return 'RARE';
-//     case 'SUPER_RARE':
-//       return 'SUPER RARE'; 
-//     case 'LEGENDARY':
-//       return 'LEGENDARY';
-//     default:
-//       throw new Error(`Invalid grade value: ${grade}`);
-//   }
-// };
+// utils/normalizeCardData.ts
+export const normalizeGrade = (grade: string): Grades => {
+  switch (grade.toUpperCase()) {
+    case 'COMMON':
+      return 'common'; 
+    case 'RARE':
+      return 'RARE';
+    case 'SUPER_RARE':
+      return 'SUPER RARE'; 
+    case 'LEGENDARY':
+      return 'LEGENDARY';
+    default:
+      throw new Error(`Invalid grade value: ${grade}`);
+  }
+};
 
-// export const normalizeGenre = (genre: string): Genres => {
-//   switch (genre.toUpperCase()) {
-//     case 'TRAVEL':
-//       return 'travel'; 
-//     case 'LANDSCAPE':
-//       return 'landscape'; 
-//     case 'PORTRAIT':
-//       return 'portrait'; 
-//     case 'OBJECT':
-//       return 'object'; 
-//     default:
-//       throw new Error(`Invalid genre value: ${genre}`);
-//   }
-// };
+export const normalizeGenre = (genre: string): Genres => {
+  switch (genre.toUpperCase()) {
+    case 'TRAVEL':
+      return 'travel'; 
+    case 'LANDSCAPE':
+      return 'landscape'; 
+    case 'PORTRAIT':
+      return 'portrait'; 
+    case 'OBJECT':
+      return 'object'; 
+    default:
+      throw new Error(`Invalid genre value: ${genre}`);
+  }
+};
 
-// export const translateGenre = (genre: Genres): string => {
-//   switch (genre) {
-//     case 'travel':
-//       return '여행';
-//     case 'landscape':
-//       return '풍경';
-//     case 'portrait':
-//       return '인물';
-//     case 'object':
-//       return '사물';
-//     default:
-//       return genre;
-//   }
-// };
+export const translateGenre = (genre: Genres): string => {
+  switch (genre) {
+    case 'travel':
+      return '여행';
+    case 'landscape':
+      return '풍경';
+    case 'portrait':
+      return '인물';
+    case 'object':
+      return '사물';
+    default:
+      return genre;
+  }
+};
