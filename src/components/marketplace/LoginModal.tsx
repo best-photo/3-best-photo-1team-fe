539050a0d8bbf2883b836d94a2d30158f528ba97
Yousuk <yousuk88@gmail.com>
2025-01-18 17:06:12 +0900

    styles: 마켓플레이스 로그인필요 모달 반응형 추가

--- src/components/marketplace/LoginModal.tsx
+++ src/components/marketplace/LoginModal.tsx
@@ -15,9 +15,9 @@ const LoginAlertModal: React.FC<LoginAlertModalProps> = ({
   return (
     <>
       <div className='fixed top-0 left-0 w-full h-full bg-[#000000CC] bg-opacity-[80] z-[50]' />
-      <div className='fixed top-1/3 left-1/2 transform -translate-x-1/2 w-[560px] h-[375px] bg-[#161616] rounded-[2px] z-[60]'>
+      <div className='fixed top-1/3 left-1/2 transform -translate-x-1/2 min-w-[345px] h-[291px] md:w-[400px] md:h-[291px] lg:w-[560px] lg:h-[375px] bg-[#161616] rounded-[2px] z-[60]'>
         <div className='flex flex-col items-center justify-center h-full'>
-          <div className='text-xl font-bold mb-4 text-white'>
+          <div className='text-[18px] lg:text-[20px] font-bold text-white'>
             로그인이 필요합니다.
           </div>
           <Image
@@ -28,13 +28,13 @@ const LoginAlertModal: React.FC<LoginAlertModalProps> = ({
             width={18}
             height={18}
           />
-          <div className='mb-4 text-[#A4A4A4] text-center mt-[40px] font-[var(--font-noto-sans-kr)] text-[16px] font-[400] leading-[23.17px] decoration-none'>
+          <div className='text-[#A4A4A4] text-center mt-[30px] lg:mt-[40px] font-noto text-[14px] lg:text-[16px] font-[400] leading-[23.17px] decoration-none'>
             로그인 하시겠습니까? <br />
             다양한 서비스를 편리하게 이용하실 수 있습니다.
           </div>
           <button
             onClick={onRedirectToLogin}
-            className='bg-[#EFFF04] text-black rounded-[2px] w-[170px] h-[60px] font-[var(--font-noto-sans-kr)] text-[18px] font-[800] leading-[26.06px] text-center decoration-none mt-[60px]'
+            className='bg-[#EFFF04] text-black rounded-[2px] w-[120px] h-[55px] md:w-[140px] md:h-[55px] lg:w-[170px] lg:h-[60px] font-noto text-[16px] lg:text-[18px] font-[800] leading-[26.06px] text-center decoration-none mt-[40px] lg:mt-[60px]'
           >
             확인
           </button>
