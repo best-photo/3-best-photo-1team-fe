5aadd1a6a8ef894cf9b17af98f8e94e1c4e10340
Yousuk <yousuk88@gmail.com>
2025-01-18 19:44:02 +0900

    style: searchInput, 메인, 마켓플레이스헤더 스타일 원래대로 원복

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -128,7 +128,7 @@ export default function Home() {
 
   return (
     <>
-      <div className='pt-[65px] md:pt-[40px] lg:pt-[60px] max-w-[1480px] px-5 lg:px-10 mx-auto'>
+      <div className='pt-[60px]'>
         <MarketplaceHeader
           isAlertVisible={isAlertVisible}
           setAlertVisible={setAlertVisible}
@@ -138,8 +138,8 @@ export default function Home() {
           setProductVisible={setProductVisible}
           onModalClose={handleModalClose}
         />
-        <div className='border-b border-white w-full mx-auto mt-[20px]'></div>
-        <div className='w-full h-[50px] flex justify-between  mx-auto mt-[20px]'>
+        <div className='border-b border-white w-[1480px] mx-auto mt-[20px]'></div>
+        <div className='w-[1480px] h-[50px] flex justify-between  mx-auto mt-[20px]'>
           <SearchSection
             key={renderKey}
             variant='marketplace'
@@ -156,7 +156,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='flex gap-[80px] flex-wrap max-w-[1480px] px-5 lg:px-10 mx-auto pt-[20px] md:pt-[40px] lg:pt-[60px] mb-[100px]'>
+      <div className='flex gap-[80px] flex-wrap w-[1480px] mx-auto pt-[60px] mb-[100px]'>
         {photoCards.length > 0 ? (
           photoCards.map((card) => (
             <PhotoCardListItem
